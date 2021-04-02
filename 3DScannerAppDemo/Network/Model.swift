b62ba74b6c1dd61703a127c1cac889d2f1e2d567
DDS-HIEUNM <hieu.nguyenminh@ddsolution.vn>
2021-04-02 22:46:07 +0700

    sau khi xem thi checkout sang nhanh khac khong duoc push code tren nhanh develop va nhanh master

---
+++ 3DScannerAppDemo/Network/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  3DScannerAppDemo
+//
+//  Created by Nguyễn Minh Hiếu on 02/04/2021.
+//
+
+import Foundation
+
+struct Model {
+    
+}
---
+++ 3DScannerAppDemo/Network/Network.swift
@@ -0,0 +1,10 @@
+//
+//  Network.swift
+//  3DScannerAppDemo
+//
+//  Created by Nguyễn Minh Hiếu on 02/04/2021.
+//
+
+import Foundation
+
+
